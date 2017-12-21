3fc9ab36b5bf0486c9764a86ea4cdcca016cc305
aniketsjagtap <aniketsjagtap@gmail.com>
2017-12-21 11:44:22 +0530

    Working to-do list

--- src/app/item.ts
+++ src/app/item.ts
@@ -1,4 +1,9 @@
 export class Item{
     id : number;
     name : string;
+
+    constructor(id,name){
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -12,17 +12,41 @@ export class ItemsComponent implements OnInit {
     id: 1,
     name: 'apple'
   };*/
-//added items property
-  items = ITEMS;
-
+ // items : ITEMS;
   selectedItem : Item;
+//added items property
+  items : Item[] = [];
+
+ 
+  addItem : any = {};
+
   
   constructor() { }
 
   ngOnInit() {
+    this.getItem();
   }
   
+  getItem() : void{
+    this.items = ITEMS;
+  }
+
   onSelect(item : Item): void{
     this.selectedItem = item;
   }
+
+  onAdd()
+  {
+    //alert("hi");
+    this.items.push(new Item(this.addItem.id,this.addItem.name));
+    console.log(this.items);
+  }
+
+  onDelete(index){
+    console.log("index"+index);
+    this.items.splice(index,1);
+    console.log(this.items);
+
+  }
+
 }
